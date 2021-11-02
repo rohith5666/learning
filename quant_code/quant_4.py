44e6d990cb612ae8dbcbb9ddee2a7f00c264b344
rohith5666 <rohithkathuroju@gmail.com>
2021-11-02 17:59:26 +0530

    version 2

--- quant_code/quant_4.py
+++ quant_code/quant_4.py
@@ -1,9 +1,12 @@
 #  A number is divisible by 4 if the number formed by its last two digits is divisible by 4.
-num = input("Enter a number:")
-x = [int(a) for a in str(num)]
+y = input("Enter a number:")
 
-if (x[-2]+x[-1])%4 == 0:
-    print(num, "is divisible by 4")
-else:
-    print(num, "is not divisible by 4")
+def div_con_4 (num):
+    x = [int(a) for a in str(num)]
 
+    if (x[-2]+x[-1])%4 == 0:
+        print(num, "is divisible by 4")
+    else:
+        print(num, "is not divisible by 4")
+
+div_con_4(y)
